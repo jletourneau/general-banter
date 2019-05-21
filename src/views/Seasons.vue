170166399fc21324c28e8d1bf2bcc248547f62db
Jack Letourneau <jletourneau@users.noreply.github.com>
2019-05-21 11:56:36 -0400

    Sorted reactions.

--- src/views/Seasons.vue
+++ src/views/Seasons.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="content">
     <h1 class="title">
-      Seasons
+      Current Season
     </h1>
     <ul>
       <li
-        v-for="season in seasons"
+        v-for="season in currentSeasons"
         :key="season.id"
       >
-        <pre>{{ season }}</pre>
+        <div>
+          {{ new Date(season.data.start) }} —
+          {{ new Date(season.data.end) }}
+        </div>
+        <div>
+          Season ID: {{ season.id }}
+        </div>
+        <div>
+          Channel ID: {{ season.data.channel.id }}
+        </div>
+        <ul>
+          <li
+            v-for="reaction in sortedReactions(season)"
+            :key="reaction.reaction"
+          >
+            {{ reaction.reaction }}: {{ reaction.count }}
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
@@ -24,7 +41,10 @@
         objects: 'objects/all',
       }),
       seasons() {
-        return this.objects('season');
+        return Object.values(this.objects('season') || {});
+      },
+      currentSeasons() {
+        return this.seasons.filter(season => this.isCurrent(season));
       },
     },
     mounted() {
@@ -34,6 +54,27 @@
       ...mapActions({
         getObjects: 'objects/lazyList',
       }),
+      isCurrent(season) {
+        const seasonStart = new Date(season.data.start);
+        const seasonEnd = new Date(season.data.end);
+        const now = new Date();
+        return (seasonStart <= now) && (seasonEnd > now);
+      },
+      sortedReactions(season) {
+        const reactions = season.aggregates.reactions.values;
+        return Object.keys(reactions).reduce((memo, reaction) => {
+          memo.push({ reaction, count: reactions[reaction] });
+          return memo;
+        }, []).sort((a, b) => {
+          if (a.reaction < b.reaction) return -1;
+          if (b.reaction < a.reaction) return 1;
+          return 0;
+        }).sort((a, b) => {
+          if (a.count < b.count) return 1;
+          if (b.count < a.count) return -1;
+          return 0;
+        });
+      },
       getSeasons() {
         this.getObjects({
           slug: 'season',
