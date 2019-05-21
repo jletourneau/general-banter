23f8d0708d363da7e6a61dd0d76fe4f6fc7ed3e6
Jack Letourneau <jletourneau@users.noreply.github.com>
2019-05-21 16:23:30 -0400

    Push more display into Leaderboard component.

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
+  <div
+    class="box"
+  >
+    <h2
+      class="title is-flex is-4"
+    >
+      <span
+        v-if="emojiFor(reaction.reaction)"
+        class="react default"
+      >
+        {{ emojiFor(reaction.reaction) }}
+      </span>
+      <img
+        v-else-if="imageFor(reaction.reaction)"
+        :src="imageFor(reaction.reaction)"
+        class="react custom"
+        alt=""
+      />
+      <span>{{ reaction.reaction }}</span>
+    </h2>
     <table class="table is-narrow is-striped is-fullwidth">
       <col width="50" />
       <col />
@@ -23,27 +42,34 @@
 <script>
   import { mapActions, mapGetters } from 'vuex';
 
+  import DEFAULT_REACTS from '@/data/emoji-mapping';
+  import CUSTOM_REACTS from '@/data/custom-mapping';
+
   export default {
     name: 'Leaderboard',
     props: {
       objectTypeSlug: { type: String, default: '' },
       leaderboardSlug: { type: String, default: '' },
-      grouping: { type: Object, default: () => {} },
       season: { type: Object, default: () => {} },
+      reaction: { type: Object, default: () => {} },
     },
     computed: {
       ...mapGetters({
         leaderboard: 'leaderboards/get',
       }),
+      leaders() {
+        return this.leaderboard(this.leaderboardParams);
+      },
       leaderboardParams() {
         return {
           objectTypeSlug: this.objectTypeSlug,
           leaderboard: this.leaderboardSlug,
-          grouping: this.grouping,
+          grouping: {
+            channel: this.season.data.channel.id,
+            season: this.season.id,
+            reaction: this.reaction.reaction,
+          },
         };
-      },
-      leaders() {
-        return this.leaderboard(this.leaderboardParams);
       },
     },
     mounted() {
@@ -56,6 +82,25 @@
       fetch() {
         this.fetchLeaderboard(this.leaderboardParams);
       },
+      emojiFor(reaction) {
+        return DEFAULT_REACTS[reaction] || null;
+      },
+      imageFor(reaction) {
+        return CUSTOM_REACTS[reaction] || null;
+      },
     },
   };
 </script>
+
+<style lang="scss" scoped>
+  .title {
+    align-items: center;
+  }
+  .react {
+    margin-right: 0.5rem;
+    font-size: 3rem;
+  }
+  .react.custom {
+    height: 3rem;
+  }
+</style>
--- src/components/SeasonLeaderboards.vue
+++ src/components/SeasonLeaderboards.vue
@@ -11,43 +11,19 @@
         {{ formatDate(season.data.end) }}
       </div>
     </div>
-    <div
+    <leaderboard
       v-for="reaction in sortedReactions(season)"
       :key="reaction.reaction"
-      class="box"
-    >
-      <h2
-        class="title is-4"
-      >
-        <span
-          v-if="emojiFor(reaction.reaction)"
-          class="react default"
-        >
-          {{ emojiFor(reaction.reaction) }}
-        </span>
-        <img
-          v-else-if="imageFor(reaction.reaction)"
-          :src="imageFor(reaction.reaction)"
-          class="react custom"
-          alt=""
-        />
-        <span>{{ reaction.reaction }}</span>
-      </h2>
-      <leaderboard
-        :grouping="groupingFor(reaction)"
-        :season="season"
-        object-type-slug="season-participant"
-        leaderboard-slug="season-leaderboard"
-      />
-    </div>
+      :season="season"
+      :reaction="reaction"
+      object-type-slug="season-participant"
+      leaderboard-slug="season-leaderboard"
+    />
   </div>
 </template>
 
 <script>
   import Leaderboard from '@/components/Leaderboard.vue';
-
-  import DEFAULT_REACTS from '@/data/emoji-mapping';
-  import CUSTOM_REACTS from '@/data/custom-mapping';
 
   export default {
     name: 'SeasonLeaderboards',
@@ -59,12 +35,6 @@
       season: { type: Object, default: () => null },
     },
     methods: {
-      emojiFor(reaction) {
-        return DEFAULT_REACTS[reaction] || null;
-      },
-      imageFor(reaction) {
-        return CUSTOM_REACTS[reaction] || null;
-      },
       formatDate(dateString) {
         const d = new Date(dateString);
         return [
@@ -99,13 +69,6 @@
             return 0;
           });
       },
-      groupingFor(reaction) {
-        return {
-          channel: this.season.data.channel.id,
-          season: this.season.id,
-          reaction: reaction.reaction,
-        };
-      },
     },
   };
 </script>
@@ -114,16 +77,5 @@
   .season-header {
     align-items: baseline;
     justify-content: space-between;
-  }
-  .title {
-    display: flex;
-    align-items: center;
-  }
-  .react {
-    margin-right: 0.5rem;
-    font-size: 3rem;
-  }
-  .react.custom {
-    height: 3rem;
   }
 </style>
